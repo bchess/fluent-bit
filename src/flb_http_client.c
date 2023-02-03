bchess/fluent-bit

commit e045d65719c20a6bbc3a3ed8b6df1e12252bcf72

http_client: handle remote 'connection' response header
Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

src/flb_http_client.c
@@ -155,7 +155,7 @@ static int check_chunked_encoding(struct flb_http_client *c)
     return FLB_HTTP_OK;
 }
 
-/* Check and set Content Length */
+/* Check response for a 'Content-Length' header */
 static int check_content_length(struct flb_http_client *c)
 {
     int ret;
@@ -188,6 +188,40 @@ static int check_content_length(struct flb_http_client *c)
 
     c->resp.content_length = atoi(tmp);
     return FLB_HTTP_OK;
+}
+
+/* Check response for a 'Connection' header */
+static int check_connection(struct flb_http_client *c)
+{
+    int ret;
+    int len;
+    const char *header;
+    char *buf;
+
+    ret = header_lookup(c, "Connection: ", 12,
+                        &header, &len);
+    if (ret == FLB_HTTP_NOT_FOUND) {
+        return FLB_HTTP_NOT_FOUND;
+    }
+
+    buf = flb_malloc(len + 1);
+    if (!buf) {
+        flb_errno();
+        return -1;
+    }
+
+    memcpy(buf, header, len);
+    buf[len] = '\0';
+
+    if (strncasecmp(buf, "close", 5) == 0) {
+        c->resp.connection_close = FLB_TRUE;
+    }
+    else if (strcasestr(buf, "keep-alive")) {
+        c->resp.connection_close = FLB_FALSE;
+    }
+    flb_free(buf);
+    return FLB_HTTP_OK;
+
 }
 
 static inline void consume_bytes(char *buf, int bytes, int length)
@@ -617,6 +651,7 @@ struct flb_http_client *flb_http_client(struct flb_upstream_conn *u_conn,
 
     /* Response */
     c->resp.content_length = -1;
+    c->resp.connection_close = -1;
 
     if ((flags & FLB_HTTP_10) == 0) {
         c->flags |= FLB_HTTP_11;
@@ -862,10 +897,10 @@ static void http_headers_destroy(struct flb_http_client *c)
     flb_kv_release(&c->headers);
 }
 
-static int flb_http_keepalive(struct flb_http_client *c)
+int flb_http_set_keepalive(struct flb_http_client *c)
 {
-    /* validate keepalive mode */
-    if ((c->flags & FLB_HTTP_KA) == 0) {
+    /* check if 'keepalive' mode is enabled in the Upstream connection */
+    if (c->u_conn->u->net.keepalive == FLB_FALSE) {
         return -1;
     }
 
@@ -973,9 +1008,6 @@ int flb_http_do(struct flb_http_client *c, size_t *bytes)
         return -1;
     }
 
-    /* Try to add keep alive header */
-    flb_http_keepalive(c);
-
     /* check enough space for the ending CRLF */
     if (header_available(c, crlf) != 0) {
         new_size = c->header_size + 2;
@@ -1073,6 +1105,23 @@ int flb_http_do(struct flb_http_client *c, size_t *bytes)
             flb_error("[http_client] broken connection to %s:%i ?",
                       c->u_conn->u->tcp_host, c->u_conn->u->tcp_port);
             return -1;
+        }
+    }
+
+    /* Check 'Connection' response header */
+    ret = check_connection(c);
+    if (ret == FLB_HTTP_OK) {
+        /*
+         * If the server replied that the connection will be closed
+         * and our Upstream connection is in keepalive mode, we must
+         * inactivate the connection.
+         */
+        if (c->resp.connection_close == FLB_TRUE) {
+            /* Do not recycle the connection (no more keepalive) */
+            flb_upstream_conn_recycle(c->u_conn, FLB_FALSE);
+            flb_debug("[http_client] server %s:%i will close connection #%i",
+                      c->u_conn->u->tcp_host, c->u_conn->u->tcp_port,
+                      c->u_conn->fd);
         }
     }
 

include/fluent-bit/flb_http_client.h
@@ -62,6 +62,7 @@ struct flb_http_response {
     int status;                /* HTTP response status          */
     int content_length;        /* Content length set by headers */
     int chunked_encoding;      /* Chunked transfer encoding ?   */
+    int connection_close;      /* connection: close ?           */
     long chunked_cur_size;
     long chunked_exp_size;     /* expected chunked size         */
     char *chunk_processed_end; /* Position to mark last chunk   */
@@ -141,6 +142,7 @@ int flb_http_add_header(struct flb_http_client *c,
                         const char *val, size_t val_len);
 int flb_http_basic_auth(struct flb_http_client *c,
                         const char *user, const char *passwd);
+int flb_http_set_keepalive(struct flb_http_client *c);
 int flb_http_set_content_encoding_gzip(struct flb_http_client *c);
 int flb_http_set_callback_context(struct flb_http_client *c,
                                   struct flb_callback *cb_ctx);

commit 05c45163d5fc9b66bf66a9a73c42275af373f51d

http_client: set Connection header for keepalive
When HTTP keepalive is desired, specify the Connection: keep-alive
header. This is a regression from e045d65719c20a6bbc3a3ed8b6df1e12252bcf72

Signed-off-by: Ben Chess <bchess@openai.com>

src/flb_http_client.c
@@ -1030,7 +1030,7 @@ static void http_headers_destroy(struct flb_http_client *c)
 int flb_http_set_keepalive(struct flb_http_client *c)
 {
     /* check if 'keepalive' mode is enabled in the Upstream connection */
-    if (flb_stream_is_keepalive(c->u_conn->stream)) {
+    if (flb_stream_is_keepalive(c->u_conn->stream) == FLB_FALSE) {
         return -1;
     }
 
@@ -1148,6 +1148,9 @@ int flb_http_do(struct flb_http_client *c, size_t *bytes)
     size_t bytes_header = 0;
     size_t bytes_body = 0;
     char *tmp;
+
+    /* Try to add keep alive header */
+    flb_http_set_keepalive(c);
 
     /* Append pending headers */
     ret = http_headers_compose(c);